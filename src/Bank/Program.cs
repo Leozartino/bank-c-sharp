aeb2b623dcba8ef82e9db3125b015a8ca49849bb
Leozartino <leozartino@gmail.com>
2020-05-29 20:19:09 -0300

    refactor: change tests in Main

--- src/Bank/Program.cs
+++ src/Bank/Program.cs
@@ -1,28 +1,23 @@
 ﻿using System;
 // using System.Collections.Generic;
 
-namespace Bank
-{
-  class Program
-  {
+namespace Bank {
+  class Program {
 
-    static void Main(string[] args)
-    {
-      Client leonardo = new Client("Leonardo", "033.377.932-07", "68299", "Avenida Castro Alves", 23);
-      Client pedro = new Client("Pedro", "053.367.932-01", "91209", "Avenida Castro Alves", 23);
+    static void Main (string[] args) {
+      Client leonardo = new Client ("Leonardo", "033.377.932-07", "68299", "Avenida Castro Alves", 23);
+      Client pedro = new Client ("Pedro", "053.367.932-01", "91209", "Avenida Castro Alves", 23);
 
-      Conta c = new Conta(25356, leonardo, 100);
-      ContaPoupanca d = new ContaPoupanca(52169, pedro, 150);
+      // Polimorfismo, podemos guardar em um array de Conta
+      // qualquer objeto do tipo Conta (incluindo classes que herdam de conta)
+      Conta[] conta = new Conta[2];
 
-      d.retira(50);
-      System.Console.WriteLine($"{d.Saldo} {c.Saldo}");
+      conta[0] = new ContaCorrente (5214, leonardo, 1500);
+      conta[1] = new ContaPoupanca (1014, pedro, 1000);
 
-      TotalizadorContas somasContas = new TotalizadorContas();
-
-      somasContas.Soma(c);
-      somasContas.Soma(d);
-
-      System.Console.WriteLine(somasContas.ValorTotal);
+      foreach (Conta c in conta) {
+        System.Console.WriteLine (c.Saldo);
+      }
 
     }
   }
