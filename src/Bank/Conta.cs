25e5f6113d6d383b64dc1ff6b2262203ec9ee042
Leozartino <leozartino@gmail.com>
2020-05-29 20:16:29 -0300

    feat: change to AbstractClass

--- src/Bank/Conta.cs
+++ src/Bank/Conta.cs
@@ -1,61 +1,38 @@
-namespace Bank
-{
+namespace Bank {
 
-  public class Conta
-  {
+  public abstract class Conta {
     protected const bool V = true;
 
     protected Client titular;
 
     protected int numero;
-    public int Numero
-    {
-      get
-      {
+    public int Numero {
+      get {
         return this.numero;
       }
-      protected set
-      {
+      protected set {
         this.numero = value;
       }
     }
     protected double saldo;
-    public double Saldo
-    {
-      get
-      {
+    public double Saldo {
+      get {
         return this.saldo;
       }
-      protected set
-      {
+      protected set {
         this.saldo = value;
       }
     }
 
-    public Conta(int numero, Client titular, double saldo)
-    {
+    public Conta (int numero, Client titular, double saldo) {
       this.numero = numero;
       this.titular = titular;
       this.saldo = saldo;
     }
-    public virtual void retira(double valor)
-    {
-      if (!this.titular.maiorDeIdade())
-      {
-        valor = 200;
-        this.saca(valor);
-      }
-      else
-      {
+    public abstract void retira (double valor);
+    public bool saca (double valor) {
 
-        this.saca(valor);
-      }
-    }
-    public bool saca(double valor)
-    {
-
-      if (this.saldo < valor)
-      {
+      if (this.saldo < valor) {
         return false;
       }
       this.saldo -= valor;
@@ -63,15 +40,10 @@ namespace Bank
 
     }
 
-    public virtual void depositaCaixa(double valor)
-    {
-      this.saldo += valor;
-    }
+    public abstract void depositaCaixa (double valor);
 
-    public void transfere(double valor, Conta contaDestino)
-    {
-      if (this.saca(valor))
-      {
+    public void transfere (double valor, Conta contaDestino) {
+      if (this.saca (valor)) {
         contaDestino.saldo += valor;
       }
     }
